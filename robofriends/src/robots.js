d44d2e051e659097e5fcea3c9f999aad22752a86
Maximiliano MB <maques.mb@gmail.com>
2022-04-05 18:49:00 -0300

    primeira parte do app react com esqueleto e funcionalidade minima

---
+++ robofriends/src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+    {
+        id: 1,
+        name:'Xablau Birolus',
+        username: 'Xablau',
+        email: 'xablau@bol.com'
+    },
+    {
+        id: 2,
+        name:'Jenna Fest',
+        username: 'Zigoto',
+        email: 'drevanu@bol.com'
+    },
+    {
+        id: 3,
+        name:'Dirigone Denda',
+        username: 'Veneno',
+        email: 'queijume@bol.com'
+    },
+    {
+        id: 4,
+        name:'Kirilo Hagh',
+        username: 'Berimba',
+        email: 'xcatapa@bol.com'
+    },
+    {
+        id: 5,
+        name:'Xablau Birolus',
+        username: 'XXenonio',
+        email: 'sigfried@bol.com'
+    },
+    {
+        id: 6,
+        name:'Finn Blot',
+        username: 'Flog',
+        email: 'biruta@bol.com'
+    },
+]
