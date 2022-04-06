94853bd04c3bd8df166d7619528d1c6a58ef8349
Maximiliano MB <maques.mb@gmail.com>
2022-04-06 15:47:13 -0300

    modificacao do titulo e adição de fonte customizada

--- robofriends/src/robots.js
+++ robofriends/src/robots.js
@@ -25,7 +25,7 @@ export const robots = [
     },
     {
         id: 5,
-        name:'Xablau Birolus',
+        name:'Xingola Varuvi',
         username: 'XXenonio',
         email: 'sigfried@bol.com'
     },
@@ -34,5 +34,41 @@ export const robots = [
         name:'Finn Blot',
         username: 'Flog',
         email: 'biruta@bol.com'
+    },
+    {
+        id: 7,
+        name:'Brol Traft',
+        username: 'Beluga',
+        email: 'jirut@bol.com'
+    },
+    {
+        id: 8,
+        name:'Leup Dreva',
+        username: 'Aria',
+        email: 'jingo@bol.com'
+    },
+    {
+        id: 9,
+        name:'Meig Lisc',
+        username: 'Kreb',
+        email: 'singrufa@bol.com'
+    },
+    {
+        id: 10,
+        name:'Purt Nekv',
+        username: 'Weard',
+        email: 'uirloga@bol.com'
+    },
+    {
+        id: 11,
+        name:'Sass Afraz',
+        username: 'Twit',
+        email: 'askol@bol.com'
+    },
+    {
+        id: 12,
+        name:'Copr Mein',
+        username: 'Zirig',
+        email: 'dumbadaba@bol.com'
     },
 ]
