a3daeeb1f678c6723d30b8440a2122663e6bf357
Maximiliano MB <maques.mb@gmail.com>
2022-04-12 11:36:30 -0300

    changing version of react

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -8,12 +8,8 @@ import './RubikPuddles-Regular.ttf';
 
 
 
-ReactDOM.render(
-  
-  <App />
-  ,
-  document.getElementById('root')
-);
+ReactDOM.createRoot(
+  document.getElementById('root')).render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
